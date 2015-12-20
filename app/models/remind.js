b39bc344ab03db83d3c56c4a09f7f24dff10669d
Joan Ngatia <joan.ngatia@andela.com>
2015-12-20 10:31:10 +0300

    Set up Get and Post methods

--- app/models/remind.js
+++ app/models/remind.js
@@ -1,9 +1,11 @@
 // app/models/remind.js
 // grab the mongoose module
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 // define our nerd model
 // module.exports allows us to pass this to other files when it is called
-module.exports = mongoose.model('Remind', {
-    name : {type : String, default: 'Task'}
-});
+var ReminderSchema = new Schema({
+    name: String
+})
+module.exports = mongoose.model('Remind', ReminderSchema);
--- server.js
+++ server.js
@@ -8,35 +8,69 @@ var methodOverride = require('method-override');
 var mongoose = require('mongoose')
 
 // configuration ===========================================
-    
+
+//allow to get data from POST method    
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 // config files
 var db = require('./config/db');
 
 // set our port
 var port = process.env.PORT || 8080; 
 
+//Routes for the API
+var router = express.Router();
+
+//middleware for all requests
+router.use(function(req, res, next){
+    console.log('We are in action');
+    next();
+});
+
+//first route
+router.get('/', function(req, res){
+    res.json({message: 'hooray!Welcome to Kumbusha!'});
+});
+
+//route to ad a new task and save it
+router.route('/tasks')
+     .post(function(req, res) {
+        
+        var remind = new Remind();      
+        remind.name = req.body.name;  
+
+        // save the task and check for errors
+        remind.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Task created!' });
+        });
+        
+    });
+
+app.use('/api', router);
+app.get('/api/:type/*/*?*', router);
 // connect to our mongoDB database 
 // (uncomment after you enter in your own credentials in config/db.js)
-mongoose.connect(db.url); 
+mongoose.connect('mongodb://localhost/kumbusha'); 
 
-// get all data/stuff of the body (POST) parameters
-// parse application/json 
-app.use(bodyParser.json()); 
+var Remind = require('./app/models/remind');
+
 
 // parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+//app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true })); 
 
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(methodOverride('X-HTTP-Method-Override')); 
+//app.use(methodOverride('X-HTTP-Method-Override')); 
 
 // set the static files location /public/img will be /img for users
-app.use(express.static(__dirname + '/public')); 
+//app.use(express.static(__dirname + '/public')); 
 
 // routes ==================================================
-require('./app/routes')(app); // configure our routes
+//require('./app/routes')(app); // configure our routes
 
 // start app ===============================================
 // startup our app at http://localhost:8080
